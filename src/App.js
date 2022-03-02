8fa00c14af4caa45fd01174eecb1f389ac1ef6b0
seilk <2018141123@yonsei.ac.kr>
2022-03-02 20:45:11 +0900

    commit_3

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          바꿔봅니다.
-          한번더.
+          김호박윤강솔
         </p>
         <a
           className="App-link"
